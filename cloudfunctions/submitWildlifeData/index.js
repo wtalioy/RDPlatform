4c63c0944bf8a4b8aaf1e4fcd63a3f1ff238d4ff
WangRuiming <23302010017@m.fudan.edu.cn>
2025-05-28 22:12:17 +0800

    Remove user contribution tracking from wildlife data submission

--- cloudfunctions/submitWildlifeData/index.js
+++ cloudfunctions/submitWildlifeData/index.js
@@ -21,44 +21,6 @@ exports.main = async (event, context) => {
         }
     });
 
-    // 获取用户贡献记录
-    const { data: userContrib } = await models.user_contributions.get({
-        filter: {
-            where: {
-                openid: {
-                    $eq: wxContext.OPENID
-                }
-            }
-        }
-    });
-
-    if (!userContrib || !userContrib.count) {
-        // 创建新用户贡献记录
-        await models.user_contributions.create({
-            data: {
-                openid: wxContext.OPENID,
-                count: 1,
-                hasUnlockedReward: false,
-            }
-        });
-    } else {
-        // 更新现有记录
-        const newCount = Number(userContrib.count) + 1;
-        await models.user_contributions.update({
-            filter: {
-                where: {
-                    openid: {
-                        $eq: wxContext.OPENID
-                    }
-                }
-            },
-            data: {
-                count: newCount,
-                hasUnlockedReward: newCount >= 10,
-            }
-        });
-    }
-
     return {
         success: true,
         data: wildlifeData
